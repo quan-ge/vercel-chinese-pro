d8012c79f069498fdc743024c881f5a68d42787f
quan-ge <quan13177379368@qq.com>
2025-08-08 18:25:22 +0800

    Add files via upload

--- vercel-chinese.user.js
+++ vercel-chinese.user.js
@@ -1,16 +1,21 @@
 // ==UserScript==
-// @name        Vercel 汉化 PRO
-// @namespace   https://github.com/quan-ge/vercel-chinese-pro
-// @description 汉化 Vercel 界面
-// @version     0.2.1
-// @author      liyixin21,quange
-// @license     GPL-3.0
-// @match       *://*.vercel.app/*
-// @match       *://vercel.com/*
-// @match       *://*.vercel.com/*
-// @icon        https://assets.vercel.com/image/upload/v1607554385/repositories/vercel/logo.png
-// @grant       none
-// @run-at      document-end
+// @name            Vercel 汉化 PRO
+// @name:en         Vercel Chinese PRO
+// @namespace       https://github.com/quan-ge/vercel-chinese-pro
+// @source          https://github.com/quan-ge/
+// @description     汉化 Vercel 界面
+// @description:en 	Unofficial Vercel Chinese Patch
+// @version         0.2.2
+// @author          liyixin21,quange
+// @license         GPL-3.0
+// @match           *://*.vercel.app/*
+// @match           *://vercel.com/*
+// @match           *://*.vercel.com/*
+// @icon            https://assets.vercel.com/image/upload/v1607554385/repositories/vercel/logo.png
+// @grant           none
+// @run-at          document-end
+// @updateURL       https://proxy.pipers.cn/https://github.com/quan-ge/vercel-chinese-pro/raw/main/vercel-chinese.user.js
+// @downloadURL     https://proxy.pipers.cn/https://github.com/quan-ge/vercel-chinese-pro/raw/main/vercel-chinese.user.js
 // ==/UserScript==
 
 (function() {
@@ -51,12 +56,14 @@
         ['You don\'t have any integration installed', '您没有安装任何集成'],
         ['Explore more integrations to expand your Vercel development experience', '探索更多集成服务，以扩展您的Vercel开发体验'],
         ['Integrations Console', '集成控制台'],
+        ['Current', '当前'],
         // 部署记录
         ['All deployments from', '所有部署来自'],
         ['Select Date Range', '选择时间段'],
         ['Provisioning', '配置中'],
+        ['for 2x more CPUs and faster builds', '获得双倍 CPU 资源和更快的构建速度'],
         // 设置
-        ['This is your team\'s visible name within Vercel. For example, the name of your company or department'， '这是您在Vercel中可见的团队名称。例如，您公司或部门的名称。'],
+        ['This is your team\'s visible name within Vercel. For example, the name of your company or department', '这是您在Vercel中可见的团队名称。例如，您公司或部门的名称。'],
         ['Please use 32 characters at maximum', '使用最多32个字符'],
         ['My', '我的'],
         ['Access Groups', '访问组'],
